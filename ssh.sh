6a51fc1c685d121aafb3ad9ab1299281898f95b0
nikhilsunilzope <138483071+nikhilsunilzope@users.noreply.github.com>
2024-04-08 14:00:00 +0530

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -23,10 +23,10 @@ options(){
 installkey(){
     cd
     mkdir -p .ssh
-    if grep -q "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDGLw8dEoJH9Ae2We67BoVtpQwqlDpq5JHbTVW7Oko6Z6ShkeAY+M93y1A2h9doXM3JSnbdqhfV7zSyQH8Rewfhq597+uyvRFyXnPCD8VU9MsIx3gCM+4UBYpMvy7h6YtIbleWEsqqFvEH3zGePIGxR8RV09m0Vr7va90PEhK0KT6not17ntew6v5POLskeIW8/fAgHLX/b0hYdDsiWIEBcy9aZoOCbUAuL2xvu2+tpZjYqMrEVqGA0QJCKpwOpb4p7AB9z8QJV46Tb8+J1HQqK+H4DMtoGjXEfVSPxMP2EZS/Og1i9Cgrru5Vmx5/ezYSVre393AxykMc7pmM5HJ7D ssh-key-2021-11-15" ".ssh/authorized_keys" -s ; then
+    if grep -q "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCdgyfLkGQD84uCvc1KRN56SOT7RhWmskbL9lr1V7oyEtLSkuW04NHtJdtZUqrzACF3mJ8HIN3YTtYre6GeGBlw4b16HxvmCPhd2RGBN1JkB6sUPH9ubb5d16Mji19QHPbUF5pOoKXSxpCSjysQGPsZkZQvKeIXGg6cW3QcV6svBw2IAtXhV5LibjIV0fHYeH2XaNXxn7t2DlyZ/9b0SQwKfqXNITbRXZptvu93BvW7RmEIgRy4NLOCJvYuY8bSWtoZMAZ5IQ+zwz5JINIohg85ADgV36NqRThPyxoyo5ZVgKBfKNuvg7twuULHN4qT3+YXIWEyDrcpASa5BsvPoU45 nikhilzope" ".ssh/authorized_keys" -s ; then
     echo "ERROR: Key is already installed, please contact me if this is not the expected output."
     else 
-    echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDGLw8dEoJH9Ae2We67BoVtpQwqlDpq5JHbTVW7Oko6Z6ShkeAY+M93y1A2h9doXM3JSnbdqhfV7zSyQH8Rewfhq597+uyvRFyXnPCD8VU9MsIx3gCM+4UBYpMvy7h6YtIbleWEsqqFvEH3zGePIGxR8RV09m0Vr7va90PEhK0KT6not17ntew6v5POLskeIW8/fAgHLX/b0hYdDsiWIEBcy9aZoOCbUAuL2xvu2+tpZjYqMrEVqGA0QJCKpwOpb4p7AB9z8QJV46Tb8+J1HQqK+H4DMtoGjXEfVSPxMP2EZS/Og1i9Cgrru5Vmx5/ezYSVre393AxykMc7pmM5HJ7D ssh-key-2021-11-15" >> ~/.ssh/authorized_keys
+    echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCdgyfLkGQD84uCvc1KRN56SOT7RhWmskbL9lr1V7oyEtLSkuW04NHtJdtZUqrzACF3mJ8HIN3YTtYre6GeGBlw4b16HxvmCPhd2RGBN1JkB6sUPH9ubb5d16Mji19QHPbUF5pOoKXSxpCSjysQGPsZkZQvKeIXGg6cW3QcV6svBw2IAtXhV5LibjIV0fHYeH2XaNXxn7t2DlyZ/9b0SQwKfqXNITbRXZptvu93BvW7RmEIgRy4NLOCJvYuY8bSWtoZMAZ5IQ+zwz5JINIohg85ADgV36NqRThPyxoyo5ZVgKBfKNuvg7twuULHN4qT3+YXIWEyDrcpASa5BsvPoU45 nikhilzope" >> ~/.ssh/authorized_keys
     echo "Key sucessfully installed"
     fi
 }
@@ -34,8 +34,8 @@ installkey(){
 removekey(){
     cd
     mkdir -p .ssh
-    if grep -q "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDGLw8dEoJH9Ae2We67BoVtpQwqlDpq5JHbTVW7Oko6Z6ShkeAY+M93y1A2h9doXM3JSnbdqhfV7zSyQH8Rewfhq597+uyvRFyXnPCD8VU9MsIx3gCM+4UBYpMvy7h6YtIbleWEsqqFvEH3zGePIGxR8RV09m0Vr7va90PEhK0KT6not17ntew6v5POLskeIW8/fAgHLX/b0hYdDsiWIEBcy9aZoOCbUAuL2xvu2+tpZjYqMrEVqGA0QJCKpwOpb4p7AB9z8QJV46Tb8+J1HQqK+H4DMtoGjXEfVSPxMP2EZS/Og1i9Cgrru5Vmx5/ezYSVre393AxykMc7pmM5HJ7D ssh-key-2021-11-15" ".ssh/authorized_keys" -s ; then
-    sed -i '/ssh-key-2021-11-15/d' .ssh/authorized_keys
+    if grep -q "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCdgyfLkGQD84uCvc1KRN56SOT7RhWmskbL9lr1V7oyEtLSkuW04NHtJdtZUqrzACF3mJ8HIN3YTtYre6GeGBlw4b16HxvmCPhd2RGBN1JkB6sUPH9ubb5d16Mji19QHPbUF5pOoKXSxpCSjysQGPsZkZQvKeIXGg6cW3QcV6svBw2IAtXhV5LibjIV0fHYeH2XaNXxn7t2DlyZ/9b0SQwKfqXNITbRXZptvu93BvW7RmEIgRy4NLOCJvYuY8bSWtoZMAZ5IQ+zwz5JINIohg85ADgV36NqRThPyxoyo5ZVgKBfKNuvg7twuULHN4qT3+YXIWEyDrcpASa5BsvPoU45 nikhilzope" ".ssh/authorized_keys" -s ; then
+    sed -i '/nikhilzope/d' .ssh/authorized_keys
     echo "Key sucessfully removed"
     else 
     echo "ERROR: Key not installed, please contact me if this is not the expected output."
